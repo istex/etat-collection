fdacc88ac3eaf7638dd3f4a930c4ee41da9919a8
MatthD <matthias.dieudonne@gmail.com>
2016-08-22 17:19:07 +0200

    Start requesting api

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const angular = require('angular');
+const angular = require('angular'),
+      request = require('request-promise');
 
 angular
   .module('app', [])
@@ -8,6 +9,24 @@ angular
 
 function appController($scope) {
   $scope.$watch('app.url', function(newValue,oldValue,scope){
-    
+    $scope.error = "";
+    if(!newValue || (newValue && (newValue.indexOf('api.istex.fr') < 0))){
+      $scope.error = 'Il ne s\'agit pas d\'un lien istex';
+      throw new Error('Il ne s\'agit pas d\'un lien istex');
+    }
+    let options = {
+      url: newValue,
+      headers: {
+        'User-Agent': 'IE13:istex-data-collection'
+      }
+    };
+    request.get(options)
+    .then(function (body) {
+
+    })
+    .catch(function (err) {
+      $scope.error = 'Impossible de telecharger le résultat de l\'api';
+      throw new Error('Impossible de telecharger le résultat de l\'api', err);
+    });
   });
 }

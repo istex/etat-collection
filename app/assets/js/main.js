abdfe70d9f4b8e37f73da39398687d9db2acc613
MatthD <matthias.dieudonne@gmail.com>
2016-08-23 09:46:02 +0200

    Can download api response

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,32 +1,40 @@
 'use strict';
 
 const angular = require('angular'),
-      request = require('request-promise');
+      request = require('request'),
+      fs = require('fs');
 
 angular
   .module('app', [])
   .controller('appController', ["$scope",appController]);
 
+var r = request.defaults({'proxy':'http://proxyout.inist.fr:8080'})
+
 function appController($scope) {
   $scope.$watch('app.url', function(newValue,oldValue,scope){
-    $scope.error = "";
-    if(!newValue || (newValue && (newValue.indexOf('api.istex.fr') < 0))){
-      $scope.error = 'Il ne s\'agit pas d\'un lien istex';
-      throw new Error('Il ne s\'agit pas d\'un lien istex');
-    }
-    let options = {
-      url: newValue,
-      headers: {
-        'User-Agent': 'IE13:istex-data-collection'
+    if (newValue !== oldValue){
+      $scope.error = "";
+      if(!newValue) return;
+      if((newValue && (newValue.indexOf('api.istex.fr') < 0))){
+        $scope.error = 'Il ne s\'agit pas d\'un lien istex';
+        return //throw new Error('Il ne s\'agit pas d\'un lien istex');
       }
-    };
-    request.get(options)
-    .then(function (body) {
-
-    })
-    .catch(function (err) {
-      $scope.error = 'Impossible de telecharger le résultat de l\'api';
-      throw new Error('Impossible de telecharger le résultat de l\'api', err);
-    });
+      let options = {
+        url: newValue,
+        headers: {
+          'User-Agent': 'ezpaarse'
+        }
+      };
+      console.log(options)
+      let result = fs.createWriteStream('test.json');
+      r
+      .get(options)
+      .on('error', function(err) {
+        console.log(err)
+        $scope.error = 'Impossible de telecharger le résultat de l\'api';
+        return
+      })
+      .pipe(result);
+    }
   });
 }

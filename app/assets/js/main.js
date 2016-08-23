e537a5f1af5d9612cf6bc6d3faff3d361a253df7
MatthD <matthias.dieudonne@gmail.com>
2016-08-23 16:15:24 +0200

    Reimport materialise compiled & can now dl json api file

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,37 +2,43 @@
 
 const angular = require('angular'),
       request = require('request'),
+      //progress = require('progress-stream'),
       fs = require('fs');
 
 angular
   .module('app', [])
   .controller('appController', ["$scope",appController]);
 
-var r = request.defaults({'proxy':'http://proxyout.inist.fr:8080'})
+var r = request.defaults({'proxy':'http://proxyout.inist.fr:8080'});
 
 function appController($scope) {
   $scope.$watch('app.url', function(newValue,oldValue,scope){
     if (newValue !== oldValue){
-      $scope.error = "";
+      console.log("test")
+      $scope.error = '';
+      $scope.bodyClass = '';
       if(!newValue) return;
       if((newValue && (newValue.indexOf('api.istex.fr') < 0))){
         $scope.error = 'Il ne s\'agit pas d\'un lien istex';
-        return //throw new Error('Il ne s\'agit pas d\'un lien istex');
+        $scope.bodyClass = 'badUrl';
+        return;
       }
       let options = {
         url: newValue,
         headers: {
-          'User-Agent': 'ezpaarse'
+          'User-Agent': 'istex-data-collection'
         }
       };
-      console.log(options)
       let result = fs.createWriteStream('test.json');
-      r
-      .get(options)
+      r.get(options)
+      .on('response', function(response) {
+        console.log(response.statusCode) // 200
+        console.log(response.headers['content-type']) // 'image/png'
+      })
       .on('error', function(err) {
         console.log(err)
         $scope.error = 'Impossible de telecharger le résultat de l\'api';
-        return
+        return;
       })
       .pipe(result);
     }

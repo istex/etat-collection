695ee13ac2cf443512b8e324892e698aebda61b4
MatthD <matthias.dieudonne@gmail.com>
2016-08-24 15:16:54 +0200

    Can now create simple csv

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,19 +2,35 @@
 
 const angular = require('angular'),
       request = require('request'),
-      //progress = require('progress-stream'),
+      path = require('path'),
+      mkdirp = require('mkdirp'),
+      sha1 = require('sha1'),
+      csv = require('json-2-csv'),
       fs = require('fs');
+
+const tmpPath = path.resolve(__dirname , '../../tmp');
+
+const optionsCsv = {
+    delimiter : {
+        wrap  : '"', // Double Quote (") character
+        field : ',', // Comma field delimiter
+        array : ';', // Semicolon array value delimiter
+        eol   : '\n' // Newline delimiter
+    },
+    prependHeader    : true,
+    sortHeader       : false,
+    trimHeaderValues : true,
+    trimFieldValues  :  true
+};
 
 angular
   .module('app', [])
   .controller('appController', ["$scope",appController]);
 
-var r = request.defaults({'proxy':'http://proxyout.inist.fr:8080'});
-
 function appController($scope) {
-  $scope.$watch('app.url', function(newValue,oldValue,scope){
+  $scope.proxy = true; //Default proxy ON
+  $scope.$watch('app.url', (newValue,oldValue,scope)=>{
     if (newValue !== oldValue){
-      console.log("test")
       $scope.error = '';
       $scope.bodyClass = '';
       if(!newValue) return;
@@ -23,24 +39,57 @@ function appController($scope) {
         $scope.bodyClass = 'badUrl';
         return;
       }
+      mkdirp(tmpPath, (err)=>{
+        if (err) {
+          $scope.error = 'Impossible de créer le dossier temp!';
+          throw new Error(err);
+        }
+      });
       let options = {
         url: newValue,
         headers: {
           'User-Agent': 'istex-data-collection'
         }
       };
-      let result = fs.createWriteStream('test.json');
+      let idReq = sha1(newValue.split('api.istex.fr')[1]),
+          result = fs.createWriteStream(tmpPath + '/' + idReq + '.json'),
+          csvPath =tmpPath + '/' + idReq + '.csv';
+
+      //CheckProxy
+      let r = $scope.proxy ? request.defaults({'proxy':'http://proxyout.inist.fr:8080'}) : request.defaults({proxy : false,tunnel: false});
+      // Request JSON
       r.get(options)
-      .on('response', function(response) {
+      .on('response', (response)=> {
         console.log(response.statusCode) // 200
         console.log(response.headers['content-type']) // 'image/png'
       })
-      .on('error', function(err) {
+      .on('error', (err)=> {
         console.log(err)
         $scope.error = 'Impossible de telecharger le résultat de l\'api';
+        throw new Error(err);
         return;
       })
       .pipe(result);
+
+      // Convert2CSV
+
+      result.on('finish', ()=>{
+        creatCSV(result.path, (csv)=>{
+          console.log(csv)
+          fs.writeFile(csvPath,csv,(err)=>{
+
+          })
+        });
+      })
     }
   });
+}
+
+function creatCSV(path,cb){
+  let obj = require(path);
+  if(obj && (!obj.aggregations)){
+    csv.json2csv(obj.hits,(err,csv)=>{
+      cb(csv)
+    }, optionsCsv);
+  }
 }
